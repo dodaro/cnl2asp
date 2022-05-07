ca8a391784d0972b85244eb5ddf49cf268d18d44
dodaro <dodaro@users.noreply.github.com>
2022-05-07 09:07:34 +0200

    Updated README Updated main.py Removed out.txt

--- src/main.py
+++ src/main.py
@@ -3,7 +3,15 @@ import sys
 from cnl.compile import CNLFile, CNLCompiler, DefinitionError
 
 if __name__ == '__main__':
-    with open("examples/HamPath", 'r') as in_file, open("out.txt", "w") as out_file:
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print("Usage python3 %s input_file [output_file]" % sys.argv[0])
+        exit(2)
+    
+    output_file = "out.txt"
+    if len(sys.argv) == 3:
+        output_file = sys.argv[2]
+
+    with open(sys.argv[1], 'r') as in_file, open(output_file, "w") as out_file:
         cnl_file: CNLFile = CNLFile(in_file)
         cnl_compiler: CNLCompiler = CNLCompiler()
         try:
