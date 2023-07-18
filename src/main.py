e7b82a817d01a36ccfc0c1feb35e0d49438817a2
simocaruso <simonecaruso998@gmail.com>
2023-07-18 17:17:55 +0200

    Added flag for syntax check

--- src/main.py
+++ src/main.py
@@ -1,21 +1,45 @@
-import sys
-
+import argparse
+import lark
 from cnl.compile import CNLFile, CNLCompiler, DefinitionError
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2 and len(sys.argv) != 3:
-        print("Usage python3 %s input_file [output_file]" % sys.argv[0])
-        exit(2)
-    
-    output_file = "out.txt"
-    if len(sys.argv) == 3:
-        output_file = sys.argv[2]
 
-    with open(sys.argv[1], 'r') as in_file, open(output_file, "w") as out_file:
-        cnl_file: CNLFile = CNLFile(in_file)
+def parse_input(in_file):
+    cnl_file = None
+    try:
+        cnl_file = CNLFile(in_file)
+    except lark.exceptions.UnexpectedInput as err:
+        print("Syntax error in input file:", err)
+    return cnl_file
+
+
+def check_syntax_only(in_file):
+    if parse_input(in_file):
+        print("Input file fits the grammar.")
+
+
+def check_syntax_and_compile(in_file, out_file):
+    cnl_file = parse_input(in_file)
+    if cnl_file:
         cnl_compiler: CNLCompiler = CNLCompiler()
         try:
             cnl_compiler.compile(cnl_file).into(out_file)
             print('Compilation completed.')
         except DefinitionError as err:
             print(str(err), 'Compilation failed.')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--check-syntax', action='store_true', help='Checks that the input fits the grammar')
+    parser.add_argument('input_file')
+    parser.add_argument('output_file', type=str, nargs='?', default='out.txt')
+    args = parser.parse_args()
+
+    input_file = args.input_file
+    output_file = args.output_file
+
+    with open(input_file, 'r') as in_file, open(output_file, "w") as out_file:
+        if args.check_syntax:
+            check_syntax_only(in_file)
+        else:
+            check_syntax_and_compile(in_file, out_file)
