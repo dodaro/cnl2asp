f1740c152296e637949b71e9ae58ed2b3b730b48
simocaruso <simonecaruso998@gmail.com>
2023-11-14 11:12:48 +0100

    Updated cnl2asp get_symbols method

--- src/cnl2asp/cnl2asp.py
+++ src/cnl2asp/cnl2asp.py
@@ -1,12 +1,13 @@
+from __future__ import annotations
 import os
 import traceback
 from typing import TextIO
-
 from lark import Lark, UnexpectedCharacters
 from lark.exceptions import VisitError
 
 from cnl2asp.ASP_elements.asp_program import ASPProgram
 from cnl2asp.exception.cnl2asp_exceptions import ParserError
+from cnl2asp.proposition.attribute_component import AttributeComponent
 from cnl2asp.proposition.entity_component import EntityComponent
 from cnl2asp.converter.asp_converter import ASPConverter
 from cnl2asp.parser.parser import CNLTransformer
@@ -15,13 +16,24 @@ from cnl2asp.proposition.signaturemanager import SignatureManager
 
 
 class Symbol:
-    def __init__(self, predicate: str, keys: list[str], attributes: list[str]):
+    def __init__(self, predicate: str, keys: list[str | Symbol], attributes: list[str | Symbol]):
+        """
+        Class for representing the concepts (ASP atoms) structure.
+
+        :param predicate:
+        :param keys: the list of attributes that are keys.
+        :param attributes: the FULL list (including the keys) of attributes of the atom.
+        """
         self.predicate = predicate
         self.keys = keys
         self.attributes = attributes
 
+    def __repr__(self):
+        return f'{self.predicate}({self.attributes})'
+
 
 class Cnl2asp:
+
     def __init__(self, input_file: TextIO):
         self.input_file = input_file
 
@@ -56,14 +68,26 @@ class Cnl2asp:
             print("Error in asp conversion:", str(e))
             return ''
 
+    def __convert_attribute(self, entity_name: str, attribute: AttributeComponent) -> str | Symbol:
+        if attribute.origin and entity_name != attribute.origin.name:
+            return Symbol(attribute.origin.name,
+                          [self.__convert_attribute(entity_name, AttributeComponent(attribute.name,
+                                                                                    attribute.value,
+                                                                                    attribute.origin.origin))],
+                          [self.__convert_attribute(entity_name, AttributeComponent(attribute.name,
+                                                                                    attribute.value,
+                                                                                    attribute.origin.origin))])
+        return attribute.name
+
     def __convert_signature(self, entity: EntityComponent) -> Symbol:
         keys = []
         attributes = []
         for attribute in entity.get_attributes():
-            attributes.append(attribute.name)
-        for key in entity.get_keys():
-            keys.append(key.name)
-        return Symbol(entity.name, keys, keys+attributes)
+            attributes.append(self.__convert_attribute(entity.name, attribute))
+        if entity.get_attributes() != entity.get_keys():
+            for key in entity.get_keys():
+                keys.append(self.__convert_attribute(entity.name, key))
+        return Symbol(entity.name, keys, keys + attributes)
 
     def get_symbols(self) -> list[Symbol]:
         self.compile()
