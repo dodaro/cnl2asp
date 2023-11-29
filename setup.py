8524cbfb82bdb31c961cff8babd83b05f8606788
simocaruso <simonecaruso998@gmail.com>
2023-11-29 16:11:06 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cnl2asp',
+    version='1.2.0',
+    description='A tool for converting a Controlled Natural Language based on English into Answer Set Programming',
+    url='https://github.com/dodaro/cnl2asp/tree/refactoring',
+    license='Apache License',
+    package_dir={'': 'src'},
+    packages=find_packages('src', exclude=['test*']),
+    install_requires=['lark'],
+    python_requires=">=3.10"
+)
