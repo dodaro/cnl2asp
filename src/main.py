fb7f0d24b793698c5751fe1d98c6ce85796c635c
simocaruso <simonecaruso998@gmail.com>
2024-01-16 18:26:30 +0100

    Added symbols flag to call get symbols method from command line

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--check-syntax', action='store_true', help='Checks that the input fits the grammar')
     parser.add_argument('--cnl2json', action='store_true', help='Gives a json format of the cnl')
+    parser.add_argument('--symbols', action='store_true', help='Gives the list of symbols')
     parser.add_argument('-p', '--print-with-functions', action='store_true',
                         help='Print atoms with functions. ' +
                              'Each attribute is converted into a function,' +
@@ -31,6 +32,8 @@ if __name__ == '__main__':
             print("Input file fits the grammar.")
     elif args.cnl2json:
         print(json.dumps(cnl2asp.cnl_to_json()))
+    elif args.symbols:
+        print(cnl2asp.get_symbols())
     else:
         asp_encoding = cnl2asp.compile()
         try:
